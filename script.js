63e7521afffb749add7a6d8420034fbae992bb96
sm-test-user <s.atyammohanty892@gmail.com>
2024-12-08 10:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "flex";
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
